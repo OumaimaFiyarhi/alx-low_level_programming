e624e7588adcce575315ec12c50355defcc21d88
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-16 07:10:13 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main is the entry
+ * main - Entry point
  *
- * this program displays a message
+ * this function displays a message
  *
- * returns always 0
+ * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
