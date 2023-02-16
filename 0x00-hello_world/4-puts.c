13010162f9c3acba02995d6c80e42e019d17187b
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-16 06:29:39 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*this function displays a message*/
-int main (void)
+/**
+ * this function displays a message
+ */
+int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
