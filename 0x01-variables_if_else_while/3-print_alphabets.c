76532e47ccbaeb5a5d20a8230a4278740f3482fa
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-17 08:56:47 -0800

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,15 @@
 
 int main(void)
 {
-	char c = 'a';
+	char c = 'a', b = 'A';
 
 	while (c <= 'z')
 	{	putchar(c);
 		c++;
+	}
+	while (b <= 'Z')
+	{	putchar(b);
+		b++;
 	}
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 if statement is for conditions
 putchar is printing whatever in it
 while is a loop
+
