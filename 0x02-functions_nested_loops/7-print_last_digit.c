da9f821a66607d51d03363de82503383051a6816
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-22 06:37:41 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: an entiger
+ *
+ * Description: 'function that prints the last digit of a number'
+ *
+ * Return: @n (Success)
+ */
+int print_last_digit(int n)
+{
+	n == n % 10;
+	return (n);
+}
