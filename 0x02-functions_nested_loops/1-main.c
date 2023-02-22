0dda82b3f42efae99e91d64c659b5d38e6e1accc
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-22 04:42:21 -0800

    task 1

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,20 +1,12 @@
-#include <unistd.h>
 #include "main.h"
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void print_alphabet(void)
-{
-	char c='a';
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	return (1);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
 }
