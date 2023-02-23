466cbf309128783fca50e2f86b0e0abc5aaf9295
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-23 12:15:03 -0800

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Description: 'more_numbers'
+ */
+void more_numbers(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 10)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
