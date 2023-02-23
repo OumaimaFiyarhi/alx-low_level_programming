2793dea5c0dc91cc889da95794dff108a4d0f870
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-23 11:26:21 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * Description: 'multiplies two integers'
+ *@a: integer 1
+ *@b: integer 2
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
