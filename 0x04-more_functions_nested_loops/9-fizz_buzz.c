307b5517575ae8010b417d04f826b81fc200c803
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-23 14:30:34 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,24 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) != 0))
+		if ((((i % 3) == 0) && ((i % 5) != 0)) && ((i != 100)))
 		{
 			printf("Fizz");
 			putchar(' ');
 		}
-		else if (((i % 5) == 0) && ((i % 3) != 0))
+		else if ((((i % 5) == 0) && ((i % 3) != 0)) && ((i != 100)))
 		{
 			printf("Buzz");
 			putchar(' ');
 		}
-		else if (((i % 3) == 0) && ((i % 5) == 0))
+		else if ((((i % 3) == 0) && ((i % 5) == 0)) && ((i != 100)))
 		{
 			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if (i == 100)
+		{
+			printf("Buzz");
 			putchar(' ');
 		}
 		else
