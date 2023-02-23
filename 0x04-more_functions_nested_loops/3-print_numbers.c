f365714acddacd5861862413bf3e6f89558a18f3
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-23 11:39:05 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,20 +3,15 @@
  * print_numbers - Entry point
  *
  * Description: 'prints numbers from 0 to 9'
- *
- * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
-	_putchar('0');
-	_putchar('1');
-	_putchar('2');
-	_putchar('3');
-	_putchar('4');
-	_putchar('5');
-	_putchar('6');
-	_putchar('7');
-	_putchar('8');
-	_putchar('9');
+	int c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
 	_putchar('\n');
 }
