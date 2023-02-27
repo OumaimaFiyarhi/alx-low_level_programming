6462ba30e77c499924392921a20c77183dae77b2
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-27 15:29:45 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - entry code
+ *@str: variable
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	while (i < j)
+	{
+		_putchar(str[i]);
+		i+=2;
+	}
+	_putchar('\n');
+}
