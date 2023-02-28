33504cb0b37c651d11d9fc4396da68138f686e4a
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-27 16:18:33 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,14 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
+	if (n <= 0)
+	{
+		return;
+	}
 	while (i < (n - 1))
 	{
 		printf("%d, ", a[i]);
 		i++;
 	}
-	printf("%d\n", a[(n-1)]);
+	printf("%d\n", a[i]);
 }
