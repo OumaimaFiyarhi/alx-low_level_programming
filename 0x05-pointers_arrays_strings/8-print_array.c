4fb2b3be3b54754436f4074e78dc544cb08045fc
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-02-27 16:10:47 -0800

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - entry code
+ *@a:integer
+ *@n: number of elements.
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < (n-1))
+	{
+		printf("%d, ", a[i]);
+		i++;
+	}
+	printf("%d\n", a[i]);
+}
