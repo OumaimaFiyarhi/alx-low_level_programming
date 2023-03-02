ab172f87cc66508e8d8418224c103f9dff7e3d6f
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-02 11:18:54 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - entry code
+ *@c: character
+ * Return: char
+ */
+
+char *cap_string(char *c)
+{
+	int i = 0;
+	int j = 1;
+
+	while (c[i] != '\0')
+	{
+		if (j == 1)
+		{
+			if((c[i] >= 'a' && c[i] <= 'z'))
+			{
+				c[i] = c[i] - 32;
+			}
+			j = 0;
+		}
+		if ((c[i] == ' ') || (c[i] == '\t') || (c[i] == '\n') || (c[i] == ',') || (c[i] == ';') || (c[i] == '.') || (c[i] == '!') || (c[i] == '?') || (c[i] == '"') || (c[i] == '(') || (c[i] == ')') || (c[i] == '{') || (c[i] == '}'))
+		{
+			j = 1;
+			
+		}
+		i++;
+	}
+	return (c);
+}
