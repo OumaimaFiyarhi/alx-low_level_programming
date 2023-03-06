4abbb2c3fd11459b62c1e11c0a57b03d8fc9e7c1
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-06 08:26:46 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcpy - prints buffer in hexa
  * @dest: destination string
  * @src: source string
- * @n: number of bytes 
+ * @n: number of bytes
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
