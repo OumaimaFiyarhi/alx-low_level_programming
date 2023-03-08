dd246ec8dff2bbcc10aa84344397988871a292d2
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-08 04:21:38 -0800

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - entry code
+ *@s: string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		i--;
+		_putchar(s[i]);
+	}
+}
