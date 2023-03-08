b09c9e6b5c2d9604ab3b2f9b447763754b69e005
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-08 10:33:36 -0800

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *prime - entry code
+ *@n: integer
+ * Return: Always 0.
+ */
+int prime (int n, int i)
+{
+	if ((i < n) && ((n % i) == 0))
+	{
+		return (0);
+	}
+	if (i == n)
+	{
+		return (1);
+	}
+	return (prime(n, i + 1));
+}
+/**
+ * is_prime_number - entry code
+ *@n: integer
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime(n, 2));
+}
