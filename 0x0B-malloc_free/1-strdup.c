234eb3595fce9167575b3cfc3a559c317bd3bed8
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-13 10:05:23 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function.
+ *@str: string
+ * Return: char.
+ */
+char *_strdup(char *str)
+{
+	int b;
+	int i;
+	char *a;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	a = malloc(sizeof(char) * (i + 1));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	b = 0;
+	while (b < (i + 1))
+	{
+		a[b] = str[b];
+		b++;
+	}
+	return (a);
+}
