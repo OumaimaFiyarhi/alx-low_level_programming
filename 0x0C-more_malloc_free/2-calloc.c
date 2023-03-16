91da4431e55b836bfc803cc46e9aba81cc8eeb77
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-16 05:40:48 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - entry code
+ *@nmemb: number of elements
+ *@size: size of bytes
+ * Return: pointer.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(nmemb * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < (nmemb *size))
+	{
+		array[i++] = 0;
+	}
+	return (array);
+}
