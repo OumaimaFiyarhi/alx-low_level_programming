852ce7f3b341feec843dfb3d66b50d8b1b55b2dc
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-03-23 10:46:38 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
-	va_start(args, n);
+	va_start(args, n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator != NULL)
+		if (separator != NULL) || (i == n - 1)
 		{
 			printf("%s", separator);
-		}
-		if (separator == NULL)
-		{
-			continue;
 		}
 	}
 	printf("\n");
