ddb7d47b2984da8694223489403d98b763159c18
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-07 14:26:37 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
-	}	
+	}
 
 	/*shiffting 1 left by index*/
 	mask = mask << index;
