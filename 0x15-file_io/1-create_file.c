aebf39d13a2189c38c43f13249192124fb7f8d59
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-10 07:49:30 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: the name of the file
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success and -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t wr;
+	int fd;
+	int i = 0;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (filename == NULL || fd == -1)
+	{
+		return (-1);
+	}
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	wr = write(fd, text_content, i);
+	if (wr == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
