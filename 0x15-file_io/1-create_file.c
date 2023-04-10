bba29c2a79deb1ae47b1a5ab3719f4600daec5e8
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-10 07:59:41 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,5 +31,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
-	return (1);
+	if (wr >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+	return (-1);
 }
