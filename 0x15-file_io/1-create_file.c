707961c8755ccdbcac83b16bd37142c920ccc2e3
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-10 08:36:49 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t wr;
+	int wr;
 	int fd;
-	ssize_t len;
+	int len;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+		for (len = 0; text_content[len]; len++)
+		{
+			continue;
+		}
 		wr = write(fd, text_content, len);
 		if (wr == -1)
 		{
@@ -31,14 +34,6 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		close(fd);
-		if (wr >= 0)
-		{
-			return (1);
-		}
-		else
-		{
-			return (-1);
-		}
 	}
-	return (-1);
+	return (1);
 }
