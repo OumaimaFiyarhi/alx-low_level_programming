b6ff8307bf0572eed373a7a2af857fe8432b1e0a
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-10 06:00:27 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints it to the SO
+ * @filename: is the name of the file
+ * @letters: is the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t ret = 0;
+	char *str;
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	ret = read(fd, str, letters);
+	if (write(1, str, ret) == -1)
+	{
+		ret = 0;
+	}
+	free(str);
+	close(fd);
+	return (ret);
+}
