6c093787155a370259beba282361c63d30867029
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-10 10:37:20 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,11 +15,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-	{
-		return (-1);
-	}
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
@@ -27,8 +22,9 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 		}
 	}
+	fd = open(filename, O_WRONLY | O_APPEND);
 	wr = write(fd, text_content, len);
-	if (wr == -1)
+	if (fd == -1 || wr == -1)
 	{
 		close(fd);
 		return (-1);
