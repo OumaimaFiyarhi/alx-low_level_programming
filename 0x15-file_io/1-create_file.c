60bdff4d4ac787c8db6e34d5c292373652683643
OumaimaFiyarhi <oumaima.fiyarhi@gmail.com>
2023-04-10 07:53:31 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int i = 0;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (filename == NULL || fd == -1)
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 	{
 		return (-1);
 	}
